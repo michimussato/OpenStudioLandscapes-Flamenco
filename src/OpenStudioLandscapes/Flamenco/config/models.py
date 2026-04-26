9746442bf668dee898fa0c15f47fe80e46184486
Michael Mussato <michimussato@gmail.com>
2026-04-26 21:07:49 +0200

    remove compute_fields. serializing them causes problems and is also not needed.

--- src/OpenStudioLandscapes/Flamenco/config/models.py
+++ src/OpenStudioLandscapes/Flamenco/config/models.py
@@ -6,7 +6,7 @@ from dagster import get_dagster_logger
 from pydantic import (
     Field,
     PositiveInt,
-    computed_field,
+    # computed_field,
 )
 
 LOGGER = get_dagster_logger(__name__)
@@ -178,7 +178,7 @@ class Config(FeatureBaseModel):
     )
 
     # EXPANDABLE PATHS
-    @computed_field
+    # @computed_field
     @property
     def flamenco_storage_expanded(self) -> pathlib.Path:
         LOGGER.debug(f"{self.env = }")
@@ -197,7 +197,7 @@ class Config(FeatureBaseModel):
         )
         return ret
 
-    @computed_field
+    # @computed_field
     @property
     def flamenco_shared_storage_expanded(self) -> pathlib.Path:
         LOGGER.debug(f"{self.env = }")
