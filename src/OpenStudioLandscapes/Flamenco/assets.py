09dd2938b1f69c75c4493a5bb64f16527ce82fa8
Michael Mussato <michimussato@gmail.com>
2026-08-05 13:10:59 +0200

    first test with docker registry as configurable resource

--- src/OpenStudioLandscapes/Flamenco/assets.py
+++ src/OpenStudioLandscapes/Flamenco/assets.py
@@ -32,6 +32,7 @@ from OpenStudioLandscapes.engine.common_assets import (
     group_out,
 )
 from OpenStudioLandscapes.engine.config.models import ConfigEngine, DockerConfigModel
+from OpenStudioLandscapes.engine.base.configurable_resources.docker_registry_resource import DockerRegistryConfigurableResource
 from OpenStudioLandscapes.engine.constants import (
     ASSET_HEADER_BASE,
     ConfigParent,
@@ -124,6 +125,7 @@ feature_in_parent: Union[AssetsDefinition, None] = group_in.get_feature_in_paren
 )
 def write_dockerfile(
     context: AssetExecutionContext,
+    config_DockerRegistryConfigurableResource: DockerRegistryConfigurableResource,
     feature_in: OpenStudioLandscapesFeatureIn,  # pylint: disable=redefined-outer-name
     CONFIG: config.models.Config,  # pylint: disable=redefined-outer-name
 ) -> Generator[Output[pathlib.Path] | AssetMaterialization, None, None]:
@@ -161,6 +163,7 @@ def write_dockerfile(
         context=context,
         docker_image=docker_image,
         docker_config=docker_config,
+        config_DockerRegistryConfigurableResource=config_DockerRegistryConfigurableResource,
         env=env,
     )
 
@@ -295,6 +298,7 @@ build_docker_image_spec = AssetSpec(
 )
 def build_docker_image(
     context: AssetExecutionContext,
+    config_DockerRegistryConfigurableResource: DockerRegistryConfigurableResource,
     feature_in: OpenStudioLandscapesFeatureIn,  # pylint: disable=redefined-outer-name
     CONFIG: config.models.Config,  # pylint: disable=redefined-outer-name
     write_dockerfile: pathlib.Path,  # pylint: disable=redefined-outer-name
@@ -326,6 +330,7 @@ def build_docker_image(
         context=context,
         docker_image=docker_image,
         docker_config=docker_config,
+        config_DockerRegistryConfigurableResource=config_DockerRegistryConfigurableResource,
         env=env,
     )
 
@@ -338,6 +343,7 @@ def build_docker_image(
         tags=tags,
         docker_image=docker_image,
         docker_config=docker_config,
+        config_DockerRegistryConfigurableResource=config_DockerRegistryConfigurableResource,
         docker_config_json=docker_config_json,
         docker_file=write_dockerfile,
     )
